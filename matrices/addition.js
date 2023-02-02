ee7062b6b69157b5cc6b12abb9ebe86d588565c5
pavan432 <pavankunchala0@gmail.com>
2023-02-03 00:41:02 +0530

    deleted comments

--- matrices/addition.js
+++ matrices/addition.js
@@ -16,14 +16,6 @@ function matrixAddition() {
     }
   }
   console.log(matrix3);
-
-  //   for (let i = 0; i < matrix1.length; i++) {
-  //     for (let j = 0; j < matrix2.length; j++) {
-  //       matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
-  //     }
-  //     console.log('\n');
-  //   }
-  //   console.log(matrix3);
 }
 
 // matrixAddition();
