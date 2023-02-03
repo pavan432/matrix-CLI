31766b65c58cddbdb8590246a9012af3bdea36c8
pavan432 <pavankunchala0@gmail.com>
2023-02-03 17:46:40 +0530

    Added Subtraction

--- matrices/subtraction.js
+++ matrices/subtraction.js
@@ -1,5 +1,25 @@
+var matrixInput = require('./matrixInput');
+
 function matrixSubtraction() {
-  console.log('Subtraction completed');
+  console.log('Enter Elements for First Matrix:');
+  var matrix1 = matrixInput();
+  console.log('Enter Elements for Second Matrix:');
+  var matrix2 = matrixInput();
+  var addMatrix = new Array(matrix1.length);
+  for (var i = 0; i < matrix1.length; i++) {
+    addMatrix[i] = new Array(matrix1[0].length);
+  }
+  for (var i = 0; i < addMatrix.length; i++) {
+    for (var j = 0; j < addMatrix[i].length; j++) {
+      addMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+    }
+  }
+  console.log('First Matrix:');
+  console.log(matrix1);
+  console.log('Second Matrix:');
+  console.log(matrix2);
+  console.log('After Subtraction:');
+  console.log(addMatrix);
 }
 
 module.exports = matrixSubtraction;
